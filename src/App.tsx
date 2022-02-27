d21d82c99086f72ceabf724faecccba7b102bb7b
RedCrafter07 <github@redcrafter07.de>
2022-02-27 17:26:15 +0100

    Live status and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,71 @@ import moment from 'moment';
 import axios from 'axios';
 import { id, token } from './env';
 
+let render = 0;
+
 const AppContent = () => {
 	const [ time, setTime ] = useState(moment().format('HH:mm:ss'));
 	const [ date, setDate ] = useState(moment().format('DD.MM.YYYY'));
+	const [ live, setLive ] = useState([
+		{
+			login: 'N/A',
+			name: 'Loading...',
+			id: 'N/A',
+			profilePicture: '',
+		},
+	]);
 	setInterval(() => {
 		setTime(moment().format('HH:mm:ss'));
 		setDate(moment().format('DD.MM.YYYY'));
 	}, 500);
+
+	if (render == 0) {
+		render++;
+		(async () => {
+			const response = await getData();
+			setLive(response);
+		})();
+		setInterval(async () => {
+			const response = await getData();
+			setLive(response);
+		}, 10 * 1000);
+	}
+
 	return (
 		<div>
 			<h1 className='text-5xl'>{time}</h1>
 			<p className='text-2xl'>{date}</p>
+			<hr className='my-12 opacity-50' />
+			<p className='text-2xl'>Live Streams</p>
+			<p className='text-xl'>
+				Below you can see the live status from the people you follow.
+			</p>
+
+			<div className='my-6' />
+
+			<div className='grid gap-4 grid-cols-8'>
+				{live.length > 0 ? (
+					live.map((v, i) => {
+						return (
+							<div key={i}>
+								<a
+									href={`https://twitch.tv/${v.login}`}
+									target={'_blank'}
+									title={v.name}
+								>
+									<img
+										src={v.profilePicture}
+										className='rounded-full w-full h-full border-4 border-red-600'
+										alt={`${v.name}'s Profile Picture`}
+									/>
+								</a>
+							</div>
+						);
+					})
+				) : (
+					'No streamers live :/'
+				)}
+			</div>
 		</div>
 	);
 };
@@ -31,8 +85,59 @@ const App = () => {
 	);
 };
 
-(async () => {
-	const res = await fetchTw('/users/follows?from_id=464418863');
+async function fetchTw(endpoint: string) {
+	const res = await axios.get(`https://api.twitch.tv/helix${endpoint}`, {
+		headers: {
+			'Client-ID': id,
+			Authorization: token,
+		},
+	});
+
+	return res.data;
+}
+
+/* function objectize(input: Array<any>, key: string) {
+	const obj: any = {};
+	input.forEach(v => {
+		if (!obj[v[key]]) obj[v[key]] = {};
+		obj[v[key]] = v;
+	});
+
+	return obj;
+} */
+
+async function getData() {
+	const res1: any = await fetchLivestreams();
+	const profilePictures: any = await getProfilePictures(
+		res1.map((v: any) => {
+			return v.user_login;
+		})
+	);
+
+	const data = res1.map((v: any, i: number) => {
+		return {
+			login: v.user_login,
+			name: v.user_name,
+			id: v.user_id,
+			profilePicture: profilePictures[i],
+		};
+	});
+
+	return data;
+}
+
+async function getProfilePictures(logins: Array<string>) {
+	const res = await fetchTw(`/users?login=${logins.join('&login=')}`);
+	let data = res.data;
+	data = data.map((v: any) => {
+		return v.profile_image_url;
+	});
+
+	return data;
+}
+
+async function fetchLivestreams() {
+	const res = await fetchTw('/users/follows?from_id=464418863&first=100');
 	const data: Array<{ to_id: string; to_login: string; to_name: string }> =
 		res.data;
 	const data2 = data.map(
@@ -44,18 +149,30 @@ const App = () => {
 			};
 		}
 	);
-	console.table(data2);
-})();
 
-async function fetchTw(endpoint: string) {
-	const res = await axios.get(`https://api.twitch.tv/helix${endpoint}`, {
-		headers: {
-			'Client-ID': id,
-			Authorization: token,
-		},
-	});
+	const res2 = await fetchTw(
+		`/streams?user_id=${data2.map(v => v.id).join('&user_id=')}&first=100`
+	);
 
-	return res.data;
+	const dataStuff: Array<{
+		game_id: string;
+		game_name: string;
+		id: string;
+		is_mature: boolean;
+		language: string;
+		started_at: string;
+		tag_ids: Array<string>;
+		thumbnail_url: string;
+		title: string;
+		type: string;
+		user_id: string;
+		user_login: string;
+		user_name: string;
+		viewer_count: number;
+	}> =
+		res2.data;
+
+	return dataStuff;
 }
 
 export default App;
